ef5c7fce2e8fe5a5314cf6f4d126aa4d511ada88
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-25 07:25:32 -0400

    Created a dictionary of seasons

---
+++ seasons.py
@@ -0,0 +1,6 @@
+seasons = {
+    "Spring": ["March", "April", "May"],
+    "Summer": ["June", "July", "August"],
+    "Fall": ["September", "October", "November"],
+    "Winter": ["December", "January", "February"]
+}
